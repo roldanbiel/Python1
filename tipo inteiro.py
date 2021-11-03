a83349e82a2974d34a4bdd0bbecf318ae31d5f86
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-03 12:42:56 -0300

    Create tipo inteiro.py

---
+++ tipo inteiro.py
@@ -0,0 +1,16 @@
+"""
+Tipo numérico
+
+OBS: São números inteiros 'definidos' por int()
+"""
+
+# números inteiros (int)
+
+# Exemplos:
+num = 42
+print(float(num))
+
+# É possível fazer dupla atribuição
+valor1, valor2 = 1, 44
+print(valor1)
+print(valor2)
